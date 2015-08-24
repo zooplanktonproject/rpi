0a5cfb8a39eff86d9cc4daa8501d1a08c003a6dc
Tony Rieker <tony@plenary.org>
2015-08-24 10:04:49 -0700

    animation test

--- run.py
+++ run.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python
 from zoo import Zoo
 from time import sleep
-from random import randint
+import json
 
 z = Zoo()
 
-def set_color(color):
 
-  for x in range(z.NODE_COUNT):
-    z.set_node(x, [color[0], color[1], color[2]])
+with open('./FQAikp95qxSyQ.json') as data_file:
+  data = json.load(data_file)
 
-  z.send_frame()
-
-
-set_color([0, 0, 0])
-sleep(1)
 
 while True:
-  color = [randint(0, 255), randint(0, 255), randint(0, 255)]
-  set_color(color)
-  sleep(1)
-  #set_color([255, 0, 0])
-  #sleep(1)
-  #set_color([0, 255, 0])
-  #sleep(1)
-  #set_color([0,0,255])
-  #sleep(1)
+  for frame in data['data']:
+    z.set_frame(frame)
+    z.send_frame()
