cad8e4decbc240a2a181e3b77d6c3e8fa6589abb
Tony Rieker <tony@plenary.org>
2015-08-29 09:55:05 -0700

    send spire every x frames

--- zoo/__init__.py
+++ zoo/__init__.py
@@ -16,9 +16,9 @@ class Zoo:
   # when we send the spire comm we check the RGB sum against this value
   # to prevent flickering
   SPIRE_SUM_CUTOFF = (255 - (255 * 0.07)) * 3
+  SPIRE_SEND_FRAME_INTERVAL = 30
 
   def __init__(self):
-    self.play_number = 0;
     self.reset_frame()
     self.frame_data = []
 
@@ -36,7 +36,7 @@ class Zoo:
     for x, colors in enumerate(self.frame):
 
       # center position is spire color, send every 30 frames
-      if x == 0:
+      if x == 0 and self.can_send_spire():
         # value is too damn high, dont want no flickerin
         if (colors[0] + colors[1] + colors[2]) > self.SPIRE_SUM_CUTOFF:
           r = self.limit_spire_bright(colors[0])
@@ -58,6 +58,8 @@ class Zoo:
     serial_array.append(self.END_CHAR)
 
     output.write("".join(serial_array))
+
+    self.frame_number += 1
 
     self.frame_delay()
 
@@ -92,6 +94,8 @@ class Zoo:
   def animate(self, filename, play_iterations):
 
     self.play_number = 0
+    self.frame_number = 0
+    self.spire_last_message_at_frame = 0
     self.reset_frame()
     self.load_frames(filename)
 
@@ -99,4 +103,13 @@ class Zoo:
       for frame in self.frame_data['data']:
         self.set_frame(frame)
         self.send_frame()
+
+  def can_send_spire(self):
+    if (self.frame_number == 0) or self.frame_number >= (self.spire_last_message_at_frame + self.SPIRE_SEND_FRAME_INTERVAL):
+
+      self.spire_last_message_at_frame = self.frame_number
+      return True
+
+    else:
+      return False
 
