6917cb3ff9dbcc45a7452e5a89ad9bc052c26c64
Tony Rieker <tony@plenary.org>
2015-09-02 19:40:33 -0700

    animation plays

--- run.py
+++ run.py
@@ -4,5 +4,8 @@ from zoo import Zoo
 z = Zoo()
 
 while True:
-
-  z.animate('ZiqUNfVR8YfsY', 10000)
+  z.animate('3o85xndeHesudYg6s0', 50)
+  z.animate('Ki5xzRu5Dt2NO', 10)
+  z.animate('TqkuOoKtmYI80', 100)
+  z.animate('fq8FnhT0UEOQw', 100)
+  z.animate('ZiqUNfVR8YfsY', 100)
