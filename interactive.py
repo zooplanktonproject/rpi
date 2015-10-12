dbe939a6071c4b779285fb13c189b63ef504e732
Tony Rieker <tony@plenary.org>
2015-10-12 11:36:52 -0700

    support for all nodes

--- interactive.py
+++ interactive.py
@@ -10,13 +10,18 @@ def cleanup():
 atexit.register(cleanup)
 
 while True:
-  pos = int(raw_input("Position: "))
+  pos = raw_input("Position (0-"+str(z.NODE_COUNT-1)+" or 'all'): ")
   r = int(raw_input("Red: "))
   g = int(raw_input("Green: "))
   b = int(raw_input("Blue: "))
 
   print pos, r, g, b
   z.reset_frame()
-  z.set_node(pos, [r, g, b])
-  z.send_frame()
+
+  if pos.lower() == 'all':
+    for i in range(z.NODE_COUNT):
+      z.set_node(i, [r, g, b])
+  else:
+    z.set_node(int(pos), [r, g, b])
+    z.send_frame()
 
