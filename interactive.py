9deec70177e1675437c538865c058283ec0ef731
Tony Rieker <tony@plenary.org>
2015-10-15 18:28:29 -0700

    continue with previously set color

--- interactive.py
+++ interactive.py
@@ -9,13 +9,23 @@ def cleanup():
 
 atexit.register(cleanup)
 
-while True:
-  pos = raw_input("Position (0-"+str(z.NODE_COUNT-1)+" or 'all'): ")
+def color_prompt():
   r = int(raw_input("Red: "))
   g = int(raw_input("Green: "))
   b = int(raw_input("Blue: "))
+  return (r, g, b)
+
+r, g, b = color_prompt()
+
+while True:
+  pos = raw_input("Position (0-"+str(z.NODE_COUNT-1)+"), or 'all', or 'c' to set a new color: ")
+
+  if pos.lower() == 'c':
+    r, g, b = color_prompt()
+    pos = raw_input("Position (0-"+str(z.NODE_COUNT-1)+"), or 'all': ")
 
   print pos, r, g, b
+
   z.reset_frame()
 
   if pos.lower() == 'all':
