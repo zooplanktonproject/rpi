e68c556260fa9e213f7c15a3937d6b4f8f47e986
Tony Rieker <tony@plenary.org>
2016-02-02 17:35:22 -0800

    udp output

--- zoo/output.py
+++ zoo/output.py
@@ -1,13 +1,32 @@
 from sys import platform as _platform
+import socket
+import json
 
-# write to serial unless on osx
+with open('./config/conf.json') as data_file:
+  conf = json.load(data_file)
+
+output = conf["output"]
+
+if output == "udp":
+
+  UDP_IP = conf["udp"]["ip"]
+  UDP_PORT = conf["udp"]["port"]
+
+  sock = socket.socket(socket.AF_INET, # Internet
+                       socket.SOCK_DGRAM) # UDP
+
 def write(str):
-  if _platform == "darwin":
+  if output == "console":
     print str
-  else:
+  elif output == "serial":
     write_serial(str)
+  elif output == "udp":
+    write_udp(str)
 
 def write_serial(str):
   import serial
   ser = serial.Serial('/dev/ttyACM0', 9600)
   ser.write(str)
+
+def write_udp(str):
+  sock.sendto(str, (UDP_IP, UDP_PORT))
